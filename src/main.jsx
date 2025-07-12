15d0e25202922f27eac9123e10bcd3180c9168eb
Goga-Chkhaidze <chxaidze.goga@list.ru>
2025-07-12 11:25:43 +0400

    tyl

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App.jsx';
 import Header from './components/Header.jsx';
-import Footer from './components/footer.jsx';
+import Footer from './components/Footer.jsx';
 import { CartProvider } from './components/CartContext.jsx';
 
 createRoot(document.getElementById('root')).render(
