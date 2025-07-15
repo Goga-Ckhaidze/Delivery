1f2b833455c7893973fc194eb68c70ebaeb490cf
Goga-Chkhaidze <chxaidze.goga@list.ru>
2025-07-15 12:01:01 +0400

    fix: update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
+import './App.css';
 import App from './App.jsx';
 import Header from './components/Header.jsx';
 import Footer from './components/Footer.jsx';
